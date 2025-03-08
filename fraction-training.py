b10717a829685bf66c17d95df6a61c35ca904493
Ziyang Yu <ziyang_yu2008@126.com>
2025-03-08 22:33:37 +0800

    add fraction-training

---
+++ fraction-training.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import numpy as np
+import time
+
+mean = 10
+std_dev = 10
+
+random_numbers = np.random.normal(loc=mean, scale=std_dev, size=1)
+test = round(abs(random_numbers[0]), 2)
+
+print(test)
+t1 = time.time()
+answer = float(input())
+t2 = time.time()
+print('multiplied:', test * answer)
+print('{t:.2f} sec used'.format(t = (t2 - t1)))
