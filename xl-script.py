f839461f779e26a29a65f0f162c62997e8fad58f
Ziyang Yu <ziyang-yu@dac.co.jp>
2025-08-14 15:39:01 +0800

    add xl-script functions

--- xl-script.py
+++ xl-script.py
@@ -6,8 +6,27 @@ from datetime import datetime
 import openpyxl
 from openpyxl.styles import Font, DEFAULT_FONT, Color, PatternFill, Border, Side
 from openpyxl.formatting.rule import FormulaRule
+from openpyxl.formatting.formatting import ConditionalFormattingList
 
 auto_open_flg = True
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def n2c(n:int):
+    """ Convert given row and column number to an Excel-style cell name. """
+    result = []
+    while n:
+        n, rem = divmod(n-1, 26)
+        result[:0] = LETTERS[rem]
+    return ''.join(result)
+def c2n(name:str):
+    """ Converts an Excel-style column name (e.g., 'A', 'B', 'AA') to its 1-based numerical index. """
+    index = 0
+    for char in name.upper():
+        index = index * 26 + (ord(char) - ord('A') + 1)
+    return index # Adjust for 0-based indexing
+
+def next_col(col) -> str:
+    return n2c(c2n(col) + 1)
 
 # styles
 RedFill = PatternFill(start_color='EE1111', end_color='EE1111', bgColor='EE1111', fill_type='solid')
@@ -48,21 +67,54 @@ def set_conditional_format(worksheet, cell_range, formula, font=DEFAULT_FONT, bo
     worksheet.conditional_formatting.add(cell_range, fr)
 scf = set_conditional_format
 
+def clear_conditional_format(worksheet):
+    '''clear conditional formatting for whole worksheet'''
+    worksheet.conditional_formatting = ConditionalFormattingList()
+
 def reflect_formula(formula:str, src:str, dst:str):
     '''generate new excel formula string based on original formula and cell'''
     return formula.upper().replace(src.upper(), dst)
 rf = reflect_formula # short cut
 
+def limit_input_formula(pos, vals:list):
+    _limit_formula = ','.join(f'not(exact({pos}, "{v}"))' for v in vals)
+    return f'AND(NOT(ISBLANK({pos})), {_limit_formula})'.upper()
+
+def not_formula(formula):
+    if formula[0] == '=':
+        return f'=not{formula[1:]}'
+    return f'=not{formula}'
+
 # ############################## ↓↓↓ script here ↓↓↓ ##############################
 
 def process_sheet(ws):
-    formula = '=AND(NOT(ISBLANK(F5)), NOT(NUMBERVALUE(F5) = 0), NOT(NUMBERVALUE(F5) = 1))'
-    formula2 = '=ISBLANK(F5)'
-    for c in ['F','G','H','I','J','K','Q','BP','BQ','BS']:
-        print(f'process col {c}')
-        for i in range(5, 3000):
-            scf(ws, f'{c}{i}', rf(formula, 'F5', f'{c}{i}'), fill=RedFill)
-            scf(ws, f'{c}{i}', rf(formula2, 'F5', f'{c}{i}'), fill=RedFill)
+
+    clear_conditional_format(ws)
+    date_formula = '=AND(NOT(ISBLANK(O8)), NOT(AND(ISNUMBER(DATEVALUE(TEXT(O8,"0000-00-00"))),LEN(O8)=8)))'
+    for i in range(5, 3000):
+        print(f'processing row {i}...\r', end='')
+
+        # check box
+        for c in ['F','G','H','I','J','K','L','Q','BP','BQ','BS']:
+            scf(ws, f'{c}{i}', f'=AND(NOT(ISBLANK(B{i})), ISBLANK({c}{i}))', fill=RedFill)
+            scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [0, 1]), fill=RedFill)
+
+        # yyyyMMdd
+        for c in ['O', 'P', 'AT']:
+            scf(ws, f'{c}{i}', rf(date_formula, 'O8', f'{c}{i}'), fill=RedFill)
+
+        c = 'AH'; scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2]), fill=RedFill)
+        c = next_col(c); scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2]), fill=RedFill)
+        c = 'AK'; scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1011]), fill=RedFill)
+        c = next_col(c); scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2, 3, 4, 5, 6, 7, 8, 99]), fill=RedFill)
+        c = next_col(c); scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2, 3]), fill=RedFill)
+        c = next_col(c); scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2, 3, 4, 5]), fill=RedFill)
+        scf(ws, f'AO{i}', f'=and(not(exact(AN{i}, "5")), ISBLANK(AO{i}))'.upper(), fill=RedFill)
+        c = 'AT'; scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1012]), fill=RedFill)
+        c = next_col(c); scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14]), fill=RedFill)
+        c = next_col(c); scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2, 3]), fill=RedFill)
+        c = 'AX'; scf(ws, f'{c}{i}', limit_input_formula(f'{c}{i}', [1, 2, 3, 4, 5]), fill=RedFill)
+
 
 def process(fp):
     wb = openpyxl.load_workbook(fp) # load workbook
@@ -80,20 +132,23 @@ def to_win_path(wsl_path):
     path = wsl_path[5:]
     return f'{path[0]}:{path[1:]}'
 
+def backup(fp):
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+    dst = f'{root}-{timestamp}{extension}'
+    shutil.copyfile(fp, dst)
+    print(f'backup to {dst}')
+
 if __name__ == '__main__':
     if len(sys.argv) < 2: print('No file given... exit'); exit(0)
     fp = sys.argv[1]
     root, extension = os.path.splitext(fp)
     if extension != '.xlsx': print('Not excel file... exit'); exit(0)
     # back up first
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H%M%S")
-    dst = f'{root}-{timestamp}{extension}'
-    shutil.copyfile(fp, dst)
-    print(f'backup to {dst}')
+    backup(fp)
 
-    print('Processing...')
+    print('Start processing...')
     process(fp)
-    print('Done')
+    print('Processed')
 
     if auto_open_flg:
         print('Opening...')
